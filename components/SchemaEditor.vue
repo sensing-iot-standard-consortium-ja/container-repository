690e8b22e01d52c5e7e54cd82b16e7f936251a33
seiketkm <seike.takumi.999@tis.co.jp>
2022-04-14 14:03:03 +0900

    start develop

---
+++ components/SchemaEditor.vue
@@ -0,0 +1,9 @@
+<template>
+  <span>hoge</span>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SchemaEditor'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <SchemaEditor />
 </template>
 
 <script lang="ts">
