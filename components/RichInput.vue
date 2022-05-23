17a922bb199ae2d465e6931af4d128b46864993d
seiketkm <seike.takumi.999@tis.co.jp>
2022-05-23 16:53:32 +0900

    wip

---
+++ components/RichInput.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="dropdown" :class="{ 'is-active': show }">
+    <div class="dropdown-trigger">
+      <button
+        class="button"
+        aria-haspopup="true"
+        aria-controls="dropdown-menu-id"
+        @click.prevent="show = !show"
+      >
+        <span>Content</span>
+        <span class="icon is-small">
+          <font-awesome-icon
+            :icon="['fas', 'fa-angle-down']"
+            aria-hidden="true"
+          />
+        </span>
+      </button>
+    </div>
+    <div class="dropdown-menu" id="dropdown-menu-id" role="menu">
+      <div class="dropdown-content">
+        <div class="dropdown-item">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Text input" />
+            </div>
+          </div>
+        </div>
+        <hr class="dropdown-divider" />
+        <div class="dropdown-item">
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control has-icons-left has-icons-right">
+              <input
+                class="input is-success"
+                type="text"
+                placeholder="Text input"
+                value="bulma"
+              />
+              <span class="icon is-small is-left">
+                <font-awesome-icon :icon="['fas', 'adjust']" />
+              </span>
+              <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+              </span>
+            </div>
+            <p class="help is-success">This username is available</p>
+          </div>
+        </div>
+        <hr class="dropdown-divider" />
+        <a href="#" class="dropdown-item"> This is a link </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { fas } from "@fortawesome/free-solid-svg-icons";
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+<style lang="sass">
+@import "bulma/bulma.sass"
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,25 @@
 export default {
-  serverMiddleware: ['~/registry'],
+  serverMiddleware: ["~/registry"],
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'iot-registory',
+    title: "iot-registory",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -31,14 +27,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
+    "@nuxtjs/fontawesome",
   ],
-
+  fontawesome: {
+    icons: {
+      solid: true,
+    },
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
